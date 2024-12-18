682e199d896f282745a806e5c1f549d8c4e19b70
Amine aichouba <aminaihouba@gmail.com>
2024-12-18 08:53:07 +0100

    first commit

---
+++ C/Data_Structures/Array/Exo_01.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+
+int main ()
+{
+    int T [10] ;
+
+    for ( int i = 0 ; i < 10 ; i ++ ) 
+        {
+            printf ( "\n\t 👉  Give me T [%d] : " , i + 1 ) ;
+            scanf ( " %d" , ( T + i ) ) ; // ( T + i ) ==  &T[i]
+        }
+
+    printf ( "\n\n\t 👉  T : " ) ;
+    for ( int i = 0 ; i < 10 ; i ++ )
+        printf ( " %d " , *( T + i ) ) ;
+    
+    printf ( "\n\n\n" ) ;
+    
+    return 0 ;
+}
